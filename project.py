d785053cf5ffd629c9558f7907706a12e109506c
claaaaassic <johnny84131@gmail.com>
2016-04-30 12:30:52 +0800

    amend Sobel function and add sobel picture
    	調整邊緣檢測函式的參數
    	輸出圖片為 row 及 col 方向合併後的圖片 ( 原為 row 方向而已 )
    
    	上傳邊緣檢測後的圖片

--- project.py
+++ project.py
@@ -5,26 +5,21 @@ import numpy as np
 img = cv2.imread("car2.jpg")
 cv2.imshow("input",img)
 
-
 # 高斯模糊
 blur = cv2.GaussianBlur(img,(5,5),0)
-cv2.imshow("gaussinan",blur)
+# cv2.imshow("gaussinan",blur)
 
 # 灰階
 gray_img = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
 
 # 邊緣檢測
-sobel_img = cv2.Sobel(gray_img,cv2.CV_8U,1,0,ksize=3)
-cv2.imshow("sobel",sobel_img)
+sobel_x = cv2.Sobel(gray_img,cv2.CV_8U,1,0)
+sobel_y = cv2.Sobel(gray_img,cv2.CV_8U,0,1)
+
+# dst = cv2.addWeighted(src1, alpha, src2, beta, gamma[, dst[, dtype]])
+# Calculates the weighted sum of two arrays.  src1 權重 alpha . src2 權重 beta.  gamma 為兩數相加之後再加上去的數
+dst = cv2.addWeighted(sobel_y,1,sobel_x,1,0)
 
 
-img_row , img_col = gray_img.shape #取得圖像的長 寬
-color = [32, 96, 160, 224] #將量化後的四個平均值放進一個陣列
-
-for row in range(img_row):
-	for col in range(img_col):
-		#做量化
-		sobel_img[row,col] = color[int(sobel_img[row,col]) / 64]
-
-cv2.imshow("quantization",sobel_img)
+cv2.imshow("sobel",dst)
 cv2.waitKey(0)
